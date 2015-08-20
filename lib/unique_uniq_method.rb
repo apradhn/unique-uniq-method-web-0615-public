3a5b0bc8568f42eab82e1b3f39b1e96da7d85c05
apradhn <abhijit.pradhan6@gmail.com>
2015-08-19 21:18:46 -0400

    completes challenge

--- lib/unique_uniq_method.rb
+++ lib/unique_uniq_method.rb
@@ -1,5 +1,14 @@
 class Array
   def unique_uniq
-    # code goes here
+    uniques = []
+    length = self.size
+    i = 0
+    loop do 
+      break if i == length
+      uniques << self[i] if i == 0
+      uniques << self[i] if uniques.include?(self[i]) == false
+      i += 1
+    end
+    uniques
   end
 end
